ba70dc2342769335290ba62b4f3fb8d057393603
Cory House <housecor@gmail.com>
2026-01-14 09:48:50 -0500

    a

---
+++ src/Menu.tsx
@@ -0,0 +1,12 @@
+export default function Menu() {
+  return (
+    <div>
+      <h1>Menu</h1>
+      <ul>
+        <li>Pizza</li>
+        <li>Pasta</li>
+        <li>Salad</li>
+      </ul>
+    </div>
+  );
+}
